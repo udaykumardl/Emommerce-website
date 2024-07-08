4a26031745f94541ba1d3dee731e4a3ed744f2de
udaykumardl <udaykumardl744@gmail.com>
2024-07-08 16:34:31 +0530

    blocks of website

---
+++ src/Products.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+
+const productsArr = [
+  {
+    title: 'Colors',
+    price: 100,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+  },
+  {
+    title: 'Black and white Colors',
+    price: 50,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+  },
+  {
+    title: 'Yellow and Black Colors',
+    price: 70,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+  },
+  {
+    title: 'Blue Color',
+    price: 100,
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+  }
+];
+
+const Products = () => {
+  return (
+    <Container>
+      <Row>
+        {productsArr.map((product, index) => (
+          <Col key={index} sm={12} md={6} lg={4} xl={3} className="mb-4">
+            <Card>
+              <Card.Img variant="top" src={product.imageUrl} />
+              <Card.Body>
+                <Card.Title>{product.title}</Card.Title>
+                <Card.Text>Price: ${product.price}</Card.Text>
+                <Button variant="primary">Add to Cart</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Products;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+bootstrap configuration
+*/
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
